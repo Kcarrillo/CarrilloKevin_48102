44b2884585e87688e4286326a6b66585d991f574
kcarrillo <kcarrillo12@student.rcc.edu>
2016-10-06 11:27:43 -0700

    Signed-off-by: kcarrillo <kcarrillo12@student.rcc.edu>

---
+++ Lab/C++Template/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Kevin Carrillo
+ * Created on October 06, 2016, 9:07 AM
+ * Purpose:  
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+//User Libraries 
+
+//Global Constants Only
+
+
+//Function Prototypes 
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables 
+    
+    //Input or initialize values 
+    
+    //Process/Calculations 
+    
+    //Output 
+   
+
+    //Exit
+    return 0;
+}
+
